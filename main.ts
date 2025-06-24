20a793d507cb021d8d5d1ea2f650e2d4f267aa00
Jinmay Jhaveri <jinmayjhaveri006@gmail.com>
2025-06-24 22:24:07 +0530

    feat: finish the project 🎊

--- main.ts
+++ main.ts
@@ -37,7 +37,7 @@ const getClipboardHistory = (): Result<string[], Error> => {
 
     lib.symbols.free_clipboard_items(arrPtr, count);
 
-    return items.reverse();
+    return items;
 };
 
 const parseArgs = async (): Promise<Result<string, Error>> => {
@@ -49,33 +49,40 @@ const parseArgs = async (): Promise<Result<string, Error>> => {
     return fileLoc;
 };
 
-const getInpOut = (
+const getIO = (
     clipHist: string[]
-): Result<{ inp: string; out: string }, Error> => {
-    let inp: string = '';
-    let out: string = '';
+): Result<{ inp: string[]; out: string[]; tcs: number }, Error> => {
+    let inp: string[] = [];
+    let out: string[] = [];
+    let tcs = -1;
 
     for (let idx = 0; idx < clipHist.length; idx++) {
-        const text = clipHist[idx];
-        if (/[0-9]/.test(text.trim().charAt(0))) {
-            const expectedLines = parseInt(text.trim().charAt(0));
-            const actualLines = text.split('\n').length;
-            if ((actualLines - 1) % expectedLines == 0) {
-                inp = text;
-                let buffOut = '';
-                if (idx - 1 >= 0) {
-                    buffOut = clipHist[idx - 1];
-                    if (buffOut.split('\n').length % expectedLines == 0) {
-                        out = buffOut;
-                    }
-                }
+        const textLines = clipHist[idx].trim().split('\n');
+
+        if (/[0-9]/.test(textLines[0].trim())) {
+            const expectedTcs = parseInt(textLines[0].trim());
+            const realNoLines = textLines.length;
+
+            if ((realNoLines - 1) % expectedTcs == 0) {
+                inp = textLines;
+                let buffOut = [];
+                // We get the latest at the front and we prefer the behind ones
                 if (idx + 1 < clipHist.length) {
-                    buffOut = clipHist[idx + 1];
-                    if (buffOut.split('\n').length % expectedLines == 0) {
+                    buffOut = clipHist[idx + 1].trim().split('\n');
+                    if (buffOut.length % expectedTcs == 0) {
                         out = buffOut;
                     }
                 }
-                if (out) break;
+                if (idx - 1 >= 0) {
+                    buffOut = clipHist[idx - 1].trim().split('\n');
+                    if (buffOut.length % expectedTcs == 0) {
+                        out = buffOut;
+                    }
+                }
+                if (out.length > 0) {
+                    tcs = expectedTcs;
+                    break;
+                }
             }
         }
     }
@@ -85,13 +92,86 @@ const getInpOut = (
             'Please make sure that the input and output have been copied to the clipboard, make a fresh copy and try again!'
         );
 
-    return { inp, out };
+    return { inp, out, tcs };
 };
 
-const exec = () => {};
+type ExecParams = { inp: string; fileLoc: string };
+const exec = async ({
+    inp,
+    fileLoc,
+}: ExecParams): Promise<Result<string, Error>> => {
+    const exec1 = 'g++';
+    const exec2 = fileLoc.replace('.cpp', '.exe');
+
+    // Compiling the file, returning compiler errors if any
+
+    const cmd1 = new Deno.Command(exec1, {
+        args: [fileLoc, '-o', exec2],
+        stderr: 'piped',
+        stdout: 'piped',
+    });
+    const { success, stderr } = await cmd1.output();
+    if (!success) return errorify(new TextDecoder().decode(stderr));
+
+    // Running the exe, simulating input, returning errors if possible or the output
+
+    const cmd2 = new Deno.Command(exec2, {
+        stderr: 'piped',
+        stdin: 'piped',
+        stdout: 'piped',
+    });
+    const child = cmd2.spawn();
+    const writer = child.stdin.getWriter();
+    await writer.write(new TextEncoder().encode(inp));
+    await writer.close();
+    const payload = await child.output();
+    if (!payload.success)
+        return errorify(new TextDecoder().decode(payload.stderr));
+
+    return new TextDecoder().decode(payload.stdout);
+};
+
+type DiffStringsParams = {
+    expected: string[];
+    received: string[];
+    tcs: number;
+};
+const printDiff = ({ expected, received, tcs }: DiffStringsParams): void => {
+    // we have to judge the number of testcase, and make that our increment count
+
+    let failedTC = 0;
+
+    const outputInc = expected.length / tcs;
+
+    for (let i = 0; i < expected.length; i += outputInc) {
+        let allMatched = true;
+        let expectedStitch = '';
+        let receivedStitch = '';
+
+        for (let k = i; k < i + outputInc; k++) {
+            expectedStitch += expected[k] + '\n';
+            receivedStitch += (received[k] ?? '<EXPECTED OUTPUT>') + '\n';
+
+            if (expected[k].trim() != received[k].trim()) {
+                allMatched = false;
+            }
+        }
+
+        if (!allMatched) {
+            failedTC++;
+            console.log(`Failed Test Case Number: ${(i % outputInc) + 1}`);
+            console.log('Expected: ');
+            console.log(expectedStitch.trim());
+            console.log('Received: ');
+            console.log(receivedStitch.trim());
+        }
+    }
+
+    console.log(`✅ (${tcs - failedTC} / ${tcs})  ❌ (${failedTC} / ${tcs}) `);
+};
 
 const isError = (inp: unknown): inp is Error =>
-    typeof inp == 'object' && inp != null && Object.hasOwn(inp, 'msg');
+    typeof inp == 'object' && inp != null && 'msg' in inp;
 
 if (import.meta.main) {
     const items = getClipboardHistory();
@@ -100,4 +180,31 @@ if (import.meta.main) {
         console.log(items.msg);
         Deno.exit();
     }
+
+    const io = getIO(items);
+
+    if (isError(io)) {
+        console.log(io.msg);
+        Deno.exit();
+    }
+
+    const fileLoc = await parseArgs();
+
+    if (isError(fileLoc)) {
+        console.log(fileLoc.msg);
+        Deno.exit();
+    }
+
+    const received = await exec({ inp: io.inp.join('\n'), fileLoc });
+
+    if (isError(received)) {
+        console.log(received.msg);
+        Deno.exit();
+    }
+
+    printDiff({
+        expected: io.out,
+        received: received.split('\n'),
+        tcs: io.tcs,
+    });
 }
